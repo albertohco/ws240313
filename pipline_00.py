d837b93787e24ff0b9c44dbe466b87a4ca473356
albertohco <acordeiro@3anet.com.br>
2024-03-13 23:36:27 -0300

    revisão pipeline e inclusão do .env

--- pipline_00.py
+++
@@ -1,9 +0,0 @@
-import os
-import gdown
-import duckdb
-import pandas as pd
-from sqlalchemy import create_engine
-from dotenv import load_dotenv
-
-# Carrega as variáveis de ambiente
-load_dotenv()
