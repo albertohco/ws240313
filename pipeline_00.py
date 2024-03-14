bd287ff993ee7ed2bc033ad5209b0e7783f06cc4
albertohco <acordeiro@3anet.com.br>
2024-03-14 00:55:43 -0300

    revisão do pipeline_00 salvar no banco de dados

--- pipeline_00.py
+++ pipeline_00.py
@@ -28,11 +28,44 @@ def listar_arquivos_csv(diretorio):
             arquivos_csv.append(caminho_completo)
     return arquivos_csv
 
+# Função para ler um arquivo CSV e retornar um DataFrame Duckdb
+
+
+def ler_csv(caminho_do_arquivo):
+    return duckdb.read_csv(caminho_do_arquivo)
+
+
+# Função para adicionar uma coluna de total de vendas
+def transformar(df):
+    # Executa a consulta SQL que inclui a nova coluna, operando sobre a tabela virtual
+    df_transformado = duckdb.sql(
+        "SELECT *, quantidade * valor AS total_vendas FROM df").df()
+    # Remove o registro da tabela virtual para limpeza
+    print(df_transformado)
+    return df_transformado
+
+# Função para converter o Duckdb em Pandas e salvar o DataFrame no PostgreSQL
+
+
+def salvar_no_postgres(df, tabela):
+    # Ex: 'postgresql://user:password@localhost:5432/database_name'
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    engine = create_engine(DATABASE_URL)
+    # Salvar o DataFrame no PostgreSQL
+    df.to_sql(tabela, con=engine, if_exists='append', index=False)
+    print("salvo no banco de dados...")
+
 
 if __name__ == "__main__":
     url_pasta = os.getenv("URL_PASTA")
     diretorio_local = './pasta_gdown'
 
-    baixar_pasta_google_drive(url_pasta, diretorio_local)
+    # baixar_pasta_google_drive(url_pasta, diretorio_local)
     arquivos_csv = listar_arquivos_csv(diretorio_local)
     print(arquivos_csv)
+    df_duckdb = ler_csv(arquivos_csv)
+    print(df_duckdb)
+    print(type(df_duckdb))
+    df = transformar(df_duckdb)
+    print(type(df))
+    salvar_no_postgres(df, "vendas_calculado")
